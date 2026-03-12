b1a3e329a696027f301c593e47ac1ac88c359582
Nayyab <153427858+Nayyab132@users.noreply.github.com>
2026-03-12 22:57:49 +0500

    Update view_ride_details.dart

--- Bicycle_Rental_App/lib/admin_panel/view_ride_details/view_ride_details.dart
+++ Bicycle_Rental_App/lib/admin_panel/view_ride_details/view_ride_details.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ViewRideDetailsScreen extends StatelessWidget {
-  const ViewRideDetailsScreen({super.key}); // Use of super.key
+  const ViewRideDetailsScreen({super.key}); 
 
   @override
   Widget build(BuildContext context) {
@@ -11,19 +11,19 @@ class ViewRideDetailsScreen extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
       ),
       body: const Padding(
-        // Use const here
-        padding: EdgeInsets.all(20.0), // EdgeInsets is a constant
+        
+        padding: EdgeInsets.all(20.0), 
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Use const here for the children
+             
               Text(
                 'Ride Details',
                 style: TextStyle(fontSize: 24),
               ),
               SizedBox(height: 20),
-              // Here you can display ride details; for now, it's just a placeholder
+             
               Text('No ride details available.'),
             ],
           ),
@@ -32,3 +32,4 @@ class ViewRideDetailsScreen extends StatelessWidget {
     );
   }
 }
+
